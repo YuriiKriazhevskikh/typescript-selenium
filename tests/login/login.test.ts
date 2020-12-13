73638899a29cf80659a844865215e83a59b80b8e
YuriiKriazhevskikh <75726152+YuriiKriazhevskikh@users.noreply.github.com>
2020-12-13 08:14:49 +0300

    Update login.test.ts

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -18,7 +18,7 @@ capabilities.set('goog:chromeOptions', {
   ]
 });
 
-xdescribe('BAD - Authorization', function () {
+describe('BAD - Authorization', function () {
   let driver: WebDriver;
 
   before(async function () {
@@ -36,15 +36,18 @@ xdescribe('BAD - Authorization', function () {
     await assert.equal(await exitAuthorizationPasswordInput, false);
   });
 
-  xit('Login with correct data', async function () {
-    await driver.get('http://lab2.webtm.ru');
-    driver.findElement(By.css('[ng-model="ctrl.email"]')).sendKeys('w@w.w');
-    driver.findElement(By.css('[ng-model="ctrl.password"]')).sendKeys('w');
-    (await driver.findElement(By.css('[type="button"]'))).click()
+  it('Login with correct data', async function () {
+    await driver.get('https://vk.com/');
     await driver.sleep(1000);
-    let authorizationPasswordInput = driver.findElement(By.css('[ng-model="ctrl.password"]'));
+    driver.findElement(By.css('#index_email')).sendKeys('123456789');
+    await driver.sleep(3000);
+    driver.findElement(By.css('#index_pass')).sendKeys('123456789');
+    await driver.sleep(3000);
+    (await driver.findElement(By.css('#index_login_button'))).click()
+    await driver.sleep(1000);
+    let authorizationPasswordInput = driver.findElement(By.css('[name="login"] [placeholder="Телефон или email"]'));
     let exitAuthorizationPasswordInput = authorizationPasswordInput.then(() => true, () => false);
-    await assert.equal(await exitAuthorizationPasswordInput, true);
+    await assert.equal(await exitAuthorizationPasswordInput, false);
   });
 
   after(() => driver && driver.quit());
